ca9fa836801d6638306d8cf16753cbdd29f2c368
arader37 <50584667+arader37@users.noreply.github.com>
2019-07-05 16:13:50 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-if(!isset($page_title)) { $page_title = 'Training'; }
+if(!isset($page_title)) { $page_title = 'Quiz Master'; }
 
 ?>
 <!DOCTYPE html>
@@ -22,7 +22,7 @@ if(!isset($page_title)) { $page_title = 'Training'; }
 </head>
 <body onload="displayAdminFields('admin1')">
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-lg ">
     
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -30,11 +30,8 @@ if(!isset($page_title)) { $page_title = 'Training'; }
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             
-            <li class="nav-item active"><a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a></li>
-            <li class="nav-item active"><a class="nav-link" href="about.html">About<span class="sr-only">(current)</span></a></li>
-            <li class="nav-item active"><a class="nav-link" href="help.html">Help<span class="sr-only">(current)</span></a></li>
-            <li class="nav-item active"><a class="nav-link" href="questions_list.php">Question List<span class="sr-only">(current)</span></a></li>
-            
+            <a href="index.php" title="SILC Quiz Master"><img id="silc" src="Images/index_images/silc_home.jpg"></a> 
+                                    
             <!-- Login / Logout Nav menu item
                Checks if there is a valid session and if so displays "logout"
                If there isn's a valid session display login. -->
@@ -46,8 +43,19 @@ if(!isset($page_title)) { $page_title = 'Training'; }
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit" name="submit-search">Search</button>
         </form>-->
         <ul class="navbar-nav mr-right">
+        <li class="nav-item active"><a class="nav-link" href="about.html">About<span class="sr-only">(current)</span></a></li>
+            <li class="nav-item active"><a class="nav-link" href="help.html">Help<span class="sr-only">(current)</span></a></li>
             <li class="nav-item">
-                
+                <?php
+                if (isset($_SESSION['role'])){
+                    echo '<li class="nav-item active"><a class="nav-link" href="questions_list.php">Lists<span class="sr-only">(current)</span></a></li>';
+                    echo '<li class="nav-item active"><a class="nav-link" href="preferences.php">preferences<span class="sr-only">(current)</span></a></li>';
+                    echo '<li class="nav-item active"><a class="nav-link" href="logout.php">Logout<span class="sr-only">(current)</span></a></li>';
+                }//end if
+                else{
+                    echo '<li class="nav-item active"><a class="nav-link" href="loginForm.php">Login<span class="sr-only">(current)</span></a></li>';
+                }//end else
+                ?>
             </li>
         </ul>
     </div>
