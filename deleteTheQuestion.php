6c5d8cafa40930f1d5398b263f1b2f50711bc78a
arader37 <50584667+arader37@users.noreply.github.com>
2019-07-24 17:48:31 -0500

    modify/delete pics

--- deleteTheQuestion.php
+++ deleteTheQuestion.php
@@ -5,7 +5,9 @@ include_once 'db_configuration.php';
 if (isset($_POST['id'])){
 
     $id = mysqli_real_escape_string($db, $_POST['id']);
+    $file = mysqli_real_escape_string($db, $_POST['image_name']);
 
+    unlink($file);
 
     $sql = "DELETE FROM questions
             WHERE id = '$id'";
--- modifyQuestion.php
+++ modifyQuestion.php
@@ -21,7 +21,7 @@ if (isset($_GET['id'])){
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo '<form action="modifyTheQuestion.php" method="POST">
+        echo '<form action="modifyTheQuestion.php" method="POST" enctype="multipart/form-data">
     <br>
     <h3>Modify Question: '.$row["topic"].' - '.$row["question"].'? </h3> <br>
     
@@ -66,8 +66,8 @@ if ($result->num_rows > 0) {
     </div>
 
     <div class="form-group col-md-4">
-      <label for="cadence">Image Path</label>
-      <input type="text" class="form-control" name="image_name" value="'.$row["image_name"].'"  maxlength="255" required>
+      <label for="cadence">New Image Path (Not Required)</label>
+      <input type="file" name="fileToUpload" id="fileToUpload" value="'.$row["image_name"].'"  maxlength="255">
     </div>
            
     <br>
